bd1010c3f9cc44b6892c8db2090776649a060bd2
Cersley <sobolyok15@gmail.com>
2025-02-16 11:51:20 +0200

    add mobile support

--- common.js
+++ common.js
@@ -21,6 +21,7 @@ var myGame = function() {
         img.style.top = Math.floor(Math.random()*401) + 'px';
         leftSide.appendChild(img);
     }
+    
   leftSideImgs = leftSide.cloneNode(true);
   leftSideImgs.removeChild(leftSideImgs.lastChild);
   rightSide.appendChild(leftSideImgs);
@@ -34,31 +35,47 @@ var myGame = function() {
     countOfRightChoise += 1;
     myGame();
   };
+
   leftSide.onclick = function() {
-    gameOver.style.display = 'block';
+    gameOver.style.display = 'flex';
     body.onclick = null;
     leftSide.lastChild.onclick = null;
     exit.style.display = 'none';
+
+    leftSide.style.visibility = 'hidden';
+    rightSide.style.visibility = 'hidden';
+    rightSide.children[0].style.visibility = 'hidden';
+
     f.innerHTML = countOfRightChoise;
   };
+
   startGame.onclick = function() {
     startGame.style.display = 'none';
     shortGame.style.display = 'block';
     longGame.style.display = 'block';
   };
+
   shortGame.onclick = function() {
     start.style.display = 'none';
     exit.style.display = 'block';
+    leftSide.style.visibility = 'visible';
+    rightSide.style.visibility = 'visible';
+    rightSide.children[0].style.visibility = 'visible';
     checkTypeOfGame = 5;
   };
   longGame.onclick = function() {
     start.style.display = 'none';
     exit.style.display = 'block';
+    leftSide.style.visibility = 'visible';
+    rightSide.style.visibility = 'visible';
+    rightSide.children[0].style.visibility = 'visible';
     checkTypeOfGame = 2;
   };
+
   var numberOfFacesAddition = function(number) {
      numberOfFaces += number;
   };
+
   function ChooseTypeOfGame(fn, num) {
     if(checkTypeOfGame === 5) {
       if(fn(num)) {
@@ -72,6 +89,7 @@ var myGame = function() {
       }
     }
   }
+
   exit.onclick = function() {
     window.location.reload();
   }
